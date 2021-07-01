ea5cc65829a2fde0668e8672c406bb3695f876a2
Nivas Muthu M G <nivasmuthumg@icloud.com>
2021-07-01 16:43:11 +0530

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Nivas Muthu M G on 01/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
